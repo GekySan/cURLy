f14215796d0171f48baee643690c270654b24e3c
Geky <gekysan@tuta.io>
2025-07-03 13:20:55 +0200

    commit initial

---
+++ LibCURL/CurlResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Curly.LibCURL
+{
+    public class CurlResult
+    {
+        public string Response { get; set; } = string.Empty;
+        public string ResponseUrl { get; set; } = string.Empty;
+        public string IpAddress { get; set; } = string.Empty;
+        public long ResponseCode { get; set; }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Curly
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Test.GetLibCurlVersionAsync();
+
+            string[] urls = new string[]
+            {
+                "https://httpbin.org/headers",
+                "https://tls.peet.ws/api/tls",
+                "https://am.i.mullvad.net/country"
+            };
+
+            string filePath = "multi-thread.txt";
+
+            await Test.RunAsync(urls, filePath, 20);
+
+            Console.WriteLine("All threads have completed.");
+        }
+    }
+}
